8bc50fe45c6077dc8e0b1110888c2a305636a4b1
Carlo <jcmcardama@gmail.com>
2022-08-10 21:07:11 +0800

    add root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    def root
+        render json: {message: "Server Running"}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#root'
   post 'user_token' => 'user_token#create'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
