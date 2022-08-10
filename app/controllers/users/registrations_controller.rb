6e6782d10541d10d49ce865a4095fd8bbe7162c6
Carlo <jcmcardama@gmail.com>
2022-08-10 22:40:45 +0800

    update sign up controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionFix
+  respond_to :json
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -11,9 +12,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    email = params[:registration][:user][:email] if params[:registration]
+    password = params[:registration][:user][:password] if params[:registration]
+    confirm_password = params[:registration][:user][:confirm_password] if params[:registration]
+    # id = User.find_by(email: email).try(:id) if email.presence
+
+    if email == "" or password == "" or confirm_password == ""
+      render status: 401, json: { error: 'The request MUST contain the user email and password.' }
+      return
+    end
+
+    build_resource(sign_up_params)
+    user = User.find_by(email: email)
+
+    if !user
+      if password == confirm_password
+        # Note that the data which should be returned depends heavily of the API client needs.
+        resource.save
+        id = User.find_by(email: email).id
+        render status: 200, json: { email: email, id: id, message: 'Sign Up successfully' }
+      else
+        render status: 401, json: { error: 'Password MUST matched' }
+      end
+    else
+      render status: 401, json: { message: 'Email is already taken' }
+    end
+  end
 
   # GET /resource/edit
   # def edit
